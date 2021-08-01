77e11b075a797e44721d47c010b8369a6d43ed42
Anderson Costa <anderson_costa_miguel@hotmail.com>
2021-08-01 13:43:26 -0300

    app desenvolvida no acompanhamento do curso des. web 2021

---
+++ app.js
@@ -0,0 +1,71 @@
+class Despesa{
+    constructor(ano,mes,dia,tipo,descricao,valor){
+        this.ano = ano
+        this.mes = mes 
+        this.dia = dia  
+        this.tipo  = tipo
+        this.descriçao = descricao
+        this.valor = valor
+    }
+
+    validarDados(){
+        for(let i in this ){
+            if(this[i] == undefined || this[i] == '' || this[i]== null){
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class BD{
+
+    constructor(){
+        let id = localStorage.getItem('id')
+        
+        if(id=== null){
+            localStorage.setItem('id',0)
+        }
+    }
+
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) +1
+    }
+    gravar(d){
+     let id = this.getProximoId()
+    localStorage.setItem(id,JSON.stringify(d))
+    localStorage.setItem('id',id)
+    }
+}
+
+let bd =  new BD()
+
+
+
+function cadastrarDespesa() {
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+let despesa = new Despesa(
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
+    valor.value,
+)
+
+
+if(despesa.validarDados()){
+    // bd.gravar(despesa)
+    $('#sucessoGravacao').modal('show')
+}else{
+   $('#erroGravacao').modal('show')
+}
+
+}
